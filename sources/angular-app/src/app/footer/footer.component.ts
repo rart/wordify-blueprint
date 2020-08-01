7c1c290720f3780dc719ea2025d363aa28bac1b4
jvega190 <jose.vega@craftersoftware.com>
2020-07-31 18:43:16 -0600

    Retrieve footer content

--- sources/angular-app/src/app/footer/footer.component.ts
+++ sources/angular-app/src/app/footer/footer.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentService } from '../content.service';
+
+export interface Footer {
+  aboutImage_s: string;
+  aboutTitle_s: string;
+  about_t: string;
+  copyright_html_raw: string;
+  quickLinksTitle_s: string;
+  socialLinksTitle_s: string;
+  quickLinks_o: {
+    label_s: string;
+    url_s: string;
+  }[];
+  craftercms: {};
+};
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +21,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  public footer: Footer;
 
-  constructor() { }
+  constructor(private contentService: ContentService) { }
 
   ngOnInit(): void {
+    this.contentService.getFooter()
+      .subscribe(response => {
+        this.footer = response;
+      });
   }
 
 }
