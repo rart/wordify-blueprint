e1a68826260d971cc37c0fdc90e2aad2990d6c32
jvega190 <jose.vega@craftersoftware.com>
2020-07-31 18:45:03 -0600

    Add footer, posts, taxonomies methods to content service

--- sources/angular-app/src/app/content.service.ts
+++ sources/angular-app/src/app/content.service.ts
@@ -5,7 +5,9 @@ import { map } from 'rxjs/operators';
 import Utils from './utils';
 import { Observable } from 'rxjs';
 import { NavItem } from './header/header.component';
-import byUrlQuery from './queries.graphql';
+import byUrlQuery, { postsQuery } from './queries.graphql';
+import { parseDescriptor } from '@craftercms/content';
+import { Footer } from './footer/footer.component';
 
 @Injectable({
   providedIn: 'root'
@@ -61,5 +63,97 @@ export class ContentService {
         ...options
       }
     )
+  }
+
+  getFooter(): Observable<Footer> {
+    return this.fetchQuery({
+      text: `query Footer {
+        component_footer {
+          items {
+            guid: objectId
+            path: localId
+            contentTypeId: content__type
+            dateCreated: createdDate_dt
+            dateModified: lastModifiedDate_dt
+            label: internal__name
+            aboutTitle_s
+            about_t
+            aboutImage_s
+            quickLinksTitle_s
+            socialLinksTitle_s
+            file__name(filter: {matches: "site-footer.xml"})
+            internal__name
+            localId
+            quickLinks_o {
+              item {
+                label_s
+                url_s
+              }
+            }
+            copyright_html_raw
+          }
+        }
+      }`
+    }).pipe(map((response: any) => parseDescriptor(response.data.component_footer.items)[0]));
+  }
+
+  getPosts(categories?, tags?, exclude?, paginationData?): Observable<any> {
+    const categoriesFilter = categories
+      ? Array.isArray(categories)
+        ? categories.map(category => ({ matches: category.key }))
+        : [{ matches: categories.key }]
+      : [];
+    const tagsFilter = tags
+      ? Array.isArray(tags)
+        ? tags.map(tag => ({ matches: tag.key }))
+        : [{ matches: tags.key }]
+      : [];
+    const queryVariables = {
+      categoriesFilter,
+      tagsFilter,
+      exclude: exclude??"",
+      ... (
+        paginationData ? {
+          limit: paginationData.itemsPerPage,
+          offset: (paginationData.currentPage * paginationData.itemsPerPage)
+        } : {}
+      )
+    }
+
+    return this.fetchQuery(
+      { text: postsQuery },
+      queryVariables
+    ).pipe(map((response: any) => ({
+      items: parseDescriptor(response.data.posts.items),
+      total: response.data.posts.total,
+      ... ( paginationData ? {
+        pageCount: Math.ceil(response.data.posts.total/paginationData.itemsPerPage)
+      }: {})
+    })));
+  }
+
+  getTaxonomies(): Observable<any> {
+    return this.fetchQuery({
+      text: `query Taxonomies {
+        taxonomy {
+          total
+          items {
+            guid: objectId
+            path: localId
+            contentTypeId: content__type
+            dateCreated: createdDate_dt
+            dateModified: lastModifiedDate_dt
+            label: internal__name
+            items {
+              item {
+                key
+                value
+                image_s
+              }
+            }
+          }
+        }
+      }`
+    }).pipe(map((response: any) => parseDescriptor(response.data.taxonomy.items)));
   }
 }
