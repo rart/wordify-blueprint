94518deae6b5aac4c4966df0ba5b01d6894551c9
jvega190 <jose.vega@craftersoftware.com>
2020-07-31 18:47:17 -0600

    Retrieve posts content from service in home component

--- sources/angular-app/src/app/home/home.component.ts
+++ sources/angular-app/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ContentService } from '../content.service';
 
 @Component({
   selector: 'app-home',
@@ -8,36 +9,27 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   @Input() state;
   public slider_o;
-  public posts_o;
   public numOfPages;
   public pageNumber;
   public bios_o;
   public categories_o;
   public tags_o;
+  public posts_o = [];
 
-  constructor() {
+  constructor(private contentService: ContentService) {
   }
 
   ngOnInit(): void {
-    // const {
-    //   model,
-    //   model: {
-    //     slider_o
-    //   },
-    //   posts,
-    //   meta: {
-    //     posts: {
-    //       total,
-    //       limit
-    //     }
-    //   }
-    // } = this.state;
     this.slider_o = this.state.model.slider_o;
-    this.posts_o = this.state.posts;
     this.bios_o = this.state.model.bios_o;
     this.numOfPages = Math.ceil(this.state.meta.posts.total / this.state.meta.posts.limit);
     this.pageNumber = 1;
     this.categories_o = this.state.taxonomies.filter(taxonomy => taxonomy.craftercms.path.endsWith('categories.xml'))[0];
     this.tags_o = this.state.taxonomies.filter(taxonomy => taxonomy.craftercms.path.endsWith('tags.xml'))[0];
+
+    this.contentService.getPosts()
+      .subscribe(response => {
+        this.posts_o = response.items;
+      });
   }
 }
